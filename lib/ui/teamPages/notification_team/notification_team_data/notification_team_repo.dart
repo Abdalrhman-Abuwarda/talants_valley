ea6edc067311048cade749e22779e01ff4b31b87
Abdalrhman Abuwarda <114994888+Abdalrhman-Abuwarda@users.noreply.github.com>
2023-04-25 15:43:13 +0300

    add notification team repo

---
+++ lib/ui/teamPages/notification_team/notification_team_data/notification_team_repo.dart
@@ -0,0 +1,8 @@
+import '../../../../core/data/network/api/dio_client.dart';
+import '../../../../locator.dart';
+
+class NotificationTeamRepo{
+
+  final DioClient  _dioClient =  locator<DioClient>();
+
+}
